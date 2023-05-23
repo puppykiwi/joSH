22e0cc01f768ec81b059084d7dcf80a230d2335d
puppykiwi <johnraymwendwa04@gmail.com>
2023-05-23 11:10:30 +0300

    Personal Shell first commit

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#define DEFAULT_BUFFER_SIZE 1024
+
+char* inputBuffer; 
+bool prompt = true;
+
+int main(int argc, char* argv[]){
+
+    inputBuffer = (char*)malloc(sizeof(char) * DEFAULT_BUFFER_SIZE); // initialize the input buffer
+
+    //TODO: implement argv mode
+    //pass all arguments to the input buffer or enter interactive mode 
+    /*
+    if (argc < 2){printf("No arguments were passed. \n");}
+    else{
+        for(int i=0; i < argc; i++){
+            char *temp = argv[i];
+            fgets(inputBuffer, sizeof(inputBuffer), *temp);
+        }
+    }
+    */
+
+   // interactive mode
+    while(prompt){
+        printf("joSH > ");
+        fgets(inputBuffer, sizeof(inputBuffer), stdin);
+        if (strncmp(inputBuffer, "exit", 4) == 0){prompt = false;}
+        else{printf("You entered: %s", inputBuffer);}
+
+    }
+    printf("%s", inputBuffer);
+
+}
